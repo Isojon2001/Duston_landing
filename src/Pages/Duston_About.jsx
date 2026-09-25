2bd68f9d241280c5453c9adebff72f077e64850c
Isojon2001 <122888565+Isojon2001@users.noreply.github.com>
2026-09-25 19:40:36 +0500

    Добавил адаптивний дизайн

--- src/Pages/Duston_About.jsx
+++ src/Pages/Duston_About.jsx
@@ -1,28 +1,46 @@
 import Footer from '../Components/Footer'
 import Rectangle43 from '../assets/image/Rectangle43.png'
+
 function Duston_About() {
   return (
-    <div className='about__video__wrapper'>
-      <div>
-        <div className='about__video'>
-          <video width="640" height="360" controls autoPlay muted>
-            <source src="/path/to/video.mp4" type="video/mp4" />
-            Ваш браузер не поддерживает тег video.
-          </video>
-        </div>
-        <div className='about__imagess'>
-          <div className='images__title'>
-            <h1>О нас</h1>
-            <p>Мы развиваем диверсифицированный портфель бизнесов в разных отраслях — от цифровых сервисов до розничной сети и логистики. Каждое направление объединяет системный подход, технологичность и фокус на качестве.
-            </p>
+    <div className="about__page">
+
+      <div className='about__video__wrapper'>
+
+        <div>
+          <div className='about__video'>
+            <video width="640" height="360" controls autoPlay muted>
+              <source src="/path/to/video.mp4" type="video/mp4" />
+              Ваш браузер не поддерживает тег video.
+            </video>
           </div>
-          <div>
-            <img src={Rectangle43} alt="" />
+
+          <div className='about__imagess'>
+
+            <div className='images__title'>
+              <h1>О нас</h1>
+
+              <p>
+                Мы развиваем диверсифицированный портфель бизнесов
+                в разных отраслях — от цифровых сервисов до розничной
+                сети и логистики. Каждое направление объединяет
+                системный подход, технологичность и фокус на качестве.
+              </p>
+            </div>
+
+            <div>
+              <img src={Rectangle43} alt="" />
+            </div>
+
           </div>
         </div>
+
       </div>
+
       <Footer />
+
     </div>
   )
 }
+
 export default Duston_About
